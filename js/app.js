dfc6729a63b4cbd7720bfb96e465fd00e613fc0f
Borkesz Máté <borkeszmate@gmail.com>
2018-06-16 11:06:03 +0200

    JS modal added

--- js/app.js
+++ js/app.js
@@ -5,3 +5,21 @@ emailSubscribeBtn.addEventListener('click', (e)=>{
  console.log(e);
  e.preventDefault();
 })
+
+
+// Pop up functionality
+
+const rotateCard= document.querySelector('.rotateCard');
+const popup = document.querySelector('.popup');
+rotateCard.addEventListener('click', (e)=>{
+ e.preventDefault();
+ popup.style.visibility ="visible";
+ popup.style.opacity = "1";
+ 
+});
+
+document.querySelector('.popup__card__closeIcon').addEventListener('click', (e)=>{
+ e.preventDefault();
+ popup.style.visibility = "hidden";
+ popup.style.opacity = "0";
+})
